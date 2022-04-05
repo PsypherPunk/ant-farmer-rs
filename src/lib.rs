426e899a12e45e6f6e0aff32010525f61e1da03b
Roger G. Coram <roger@thedatashed.co.uk>
2022-04-05 13:06:49 +0100

    feat: handle CONSTRAINTs
    - refactor to work on an underlying `Vec<Vec<String>>`—we're only
      interested in aligning each "column";
    - include contraints in the formatting.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,100 @@
 use sqlparser::{
-    ast::{ColumnDef, ColumnOption, Statement},
+    ast::{ColumnDef, ColumnOption, Statement, TableConstraint},
     dialect::Dialect,
     parser::{Parser, ParserError},
 };
+
+trait AlignedDisplay {
+    fn segments(&self) -> Vec<String>;
+}
+
+/// Holds the components of a constraint definition about which we care for
+/// display purposes:
+///
+/// ```sql
+/// CREATE TABLE table_name (
+///   , CONSTRAINT NAME   FOREIGN KEY       (COLUMN)   REFERENCES TARGET_TABLE   (TARGET_COLUMN)
+///   , CONSTRAINT {name} {constraint_type} ({column}) REFERENCES {target_table} ({target_column})
+/// )
+/// ;
+/// ```
+impl AlignedDisplay for TableConstraint {
+    fn segments(&self) -> Vec<String> {
+        match self {
+            TableConstraint::Unique {
+                name,
+                columns,
+                is_primary,
+            } => {
+                vec![
+                    format!("CONSTRAINT {}", name.clone().unwrap().to_string()),
+                    if *is_primary {
+                        "PRIMARY KEY".to_string()
+                    } else {
+                        "UNIQUE".to_string()
+                    },
+                    columns
+                        .iter()
+                        .map(|column| column.to_string())
+                        .collect::<Vec<_>>()
+                        .join(", "),
+                    "".to_string(),
+                    "".to_string(),
+                    "".to_string(),
+                    "".to_string(),
+                    "".to_string(),
+                ]
+            }
+            TableConstraint::ForeignKey {
+                name,
+                columns,
+                foreign_table,
+                referred_columns,
+                on_delete,
+                on_update,
+            } => {
+                vec![
+                    format!("CONSTRAINT {}", name.clone().unwrap().to_string()),
+                    "FOREIGN KEY".to_string(),
+                    columns
+                        .iter()
+                        .map(|column| column.to_string())
+                        .collect::<Vec<_>>()
+                        .join(", "),
+                    "REFERENCES".to_string(),
+                    foreign_table.to_string(),
+                    referred_columns
+                        .iter()
+                        .map(|column| column.to_string())
+                        .collect::<Vec<_>>()
+                        .join(", "),
+                    if let Some(action) = on_delete {
+                        format!("ON DELETE {}", action)
+                    } else {
+                        "".to_string()
+                    },
+                    if let Some(action) = on_update {
+                        format!("ON UPDATE {}", action)
+                    } else {
+                        "".to_string()
+                    },
+                ]
+            }
+            TableConstraint::Check { name, expr } => {
+                vec![
+                    format!("CONSTRAINT {}", name.clone().unwrap().to_string()),
+                    format!("CHECK ({})", expr),
+                    "".to_string(),
+                    "".to_string(),
+                    "".to_string(),
+                    "".to_string(),
+                    "".to_string(),
+                    "".to_string(),
+                ]
+            }
+        }
+    }
+}
 
 /// Holds the components of a column definition about which we care for display
 /// purposes:
@@ -14,31 +106,34 @@ use sqlparser::{
 /// )
 /// ;
 /// ```
-struct DisplayColumn {
-    name: String,
-    data_type: String,
-    options: (String, String),
-}
+impl AlignedDisplay for ColumnDef {
+    fn segments(&self) -> Vec<String> {
+        let nullable = match self
+            .options
+            .iter()
+            .map(|option| &option.option)
+            .find(|option| {
+                matches!(option, ColumnOption::Null) || matches!(option, ColumnOption::NotNull)
+            }) {
+            Some(option) => option.to_string(),
+            None => "".to_string(),
+        };
+        let default = match self
+            .options
+            .iter()
+            .map(|option| &option.option)
+            .find(|option| matches!(option, ColumnOption::Default(_)))
+        {
+            Some(option) => option.to_string(),
+            None => "".to_string(),
+        };
 
-impl From<&ColumnDef> for DisplayColumn {
-    /// Convert a `ColumnDef` to its component, display-relevant parts.
-    fn from(column: &ColumnDef) -> Self {
-        Self {
-            name: column.name.value.to_owned(),
-            data_type: column.data_type.to_string(),
-            options: column
-                .options
-                .iter()
-                .fold(("".to_owned(), "".to_owned()), |acc, option| {
-                    match option.option {
-                        ColumnOption::Null | ColumnOption::NotNull => {
-                            (option.option.to_string(), acc.1)
-                        }
-                        ColumnOption::Default(_) => (acc.0, option.option.to_string()),
-                        _ => todo!(),
-                    }
-                }),
-        }
+        vec![
+            self.name.to_string(),
+            self.data_type.to_string(),
+            nullable,
+            default,
+        ]
     }
 }
 
@@ -56,37 +151,9 @@ impl<T: Dialect> From<T> for AntFarmer<T> {
 }
 
 impl<T: Dialect> AntFarmer<T> {
-    fn get_column_strings(&self, columns: &[ColumnDef]) -> Vec<DisplayColumn> {
-        columns
-            .iter()
-            .map(|column| column.into())
-            .collect::<Vec<_>>()
-    }
-
-    fn get_column_max_widths(&self, columns: &[DisplayColumn]) -> (usize, usize, usize, usize) {
-        columns
-            .iter()
-            .map(|column| {
-                (
-                    column.name.len(),
-                    column.data_type.len(),
-                    column.options.0.len(),
-                    column.options.1.len(),
-                )
-            })
-            .fold((0, 0, 0, 0), |acc, (name, type_, null, default)| {
-                (
-                    name.max(acc.0),
-                    type_.max(acc.1),
-                    null.max(acc.2),
-                    default.max(acc.3),
-                )
-            })
-    }
-
     /// Parses the input SQL and outputs our "correctly" formatted version.
     ///
-    /// Curretly only `CREATE TABLE` is supported.
+    /// Currently only `CREATE TABLE` is supported.
     pub fn mierenneuke(&self, sql: &str) -> Result<String, ParserError> {
         let ast = Parser::parse_sql(&self.dialect, sql)?;
 
@@ -94,26 +161,96 @@ impl<T: Dialect> AntFarmer<T> {
 
         for statement in ast.iter() {
             match statement {
-                Statement::CreateTable { name, columns, .. } => {
+                Statement::CreateTable {
+                    name,
+                    columns,
+                    constraints,
+                    ..
+                } => {
                     output += &format!("CREATE TABLE {} (\n", name);
 
-                    let column_strings = self.get_column_strings(columns);
+                    let columns = columns
+                        .iter()
+                        .map(|column| column.segments())
+                        .collect::<Vec<_>>();
 
-                    let (name_width, type_width, null_width, default_width) =
-                        self.get_column_max_widths(&column_strings);
+                    let constraints = constraints
+                        .iter()
+                        .map(|constraint| constraint.segments())
+                        .collect::<Vec<_>>();
 
-                    let columns =
-                    column_strings
+                    let column_widths = columns.iter().fold((0, 0, 0, 0), |acc, column| {
+                        (
+                            acc.0.max(column[0].len()),
+                            acc.1.max(column[1].len()),
+                            acc.2.max(column[2].len()),
+                            acc.3.max(column[3].len()),
+                        )
+                    });
+                    let constraint_widths =
+                        constraints
+                            .iter()
+                            .fold((0, 0, 0, 0, 0, 0, 0, 0), |acc, column| {
+                                (
+                                    acc.0.max(column[0].len()),
+                                    acc.1.max(column[1].len()),
+                                    acc.2.max(column[2].len()),
+                                    acc.3.max(column[3].len()),
+                                    acc.4.max(column[4].len()),
+                                    acc.5.max(column[5].len()),
+                                    acc.6.max(column[6].len()),
+                                    acc.7.max(column[7].len()),
+                                )
+                            });
+
+                    let columns = columns
                         .iter()
                         .map(|column| {
                             format!(
                                 "{:<name_width$} {:<type_width$} {:>null_width$} {:<default_width$}",
-                                column.name, column.data_type, column.options.0, column.options.1,
+                                column[0], column[1], column[2], column[3],
+                                name_width=column_widths.0,
+                                type_width=column_widths.1,
+                                null_width=column_widths.2,
+                                default_width=column_widths.3,
                             )
                         })
                         .collect::<Vec<_>>()
                         .join("\n  , ");
-                    output += &format!("    {}\n)\n;", columns);
+
+                    let constraints = constraints
+                        .iter()
+                        .map(|constraint| {
+                            format!(
+                                "{:<name_width$} {:<type_width$} {:<columns_width$} {:<three$} {:<four$} {:<five$} {:<six$} {:<seven$}",
+                                constraint[0],
+                                constraint[1],
+                                format!("({})", constraint[2]),
+                                constraint[3],
+                                constraint[4],
+                                if constraint[5].len() > 0 { format!("({})", constraint[5]) } else { "".to_owned() },
+                                constraint[6],
+                                constraint[7],
+                                name_width=constraint_widths.0,
+                                type_width=constraint_widths.1,
+                                columns_width=constraint_widths.2 + 2,
+                                three=constraint_widths.3,
+                                four=constraint_widths.4,
+                                five=constraint_widths.5 + 2,
+                                six=constraint_widths.6,
+                                seven=constraint_widths.7,
+                            )
+                            .trim()
+                            .to_owned()
+                        })
+                        .collect::<Vec<_>>()
+                        .join("\n  , ");
+
+                    output += &format!("    {}\n", columns);
+                    if constraints.len() > 0 {
+                        output += &format!("  , {}\n", constraints);
+                    }
+                    output += ")\n;";
                 }
                 _ => todo!(),
             }
@@ -155,6 +292,26 @@ mod tests {
   , operator_ip_address_id INT(11)  NOT NULL                            
   , create_consumers       JSON         NULL                            
   , created_date           datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()
+)
+;"#;
+
+        let result = ant_farmer.mierenneuke(sql).unwrap();
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_create_table_constraints() {
+        let sql = r#"cReAtE tAbLe operators_create_consumers (operator_api_key_id    int(11)    NOT NULL, operator_ip_address_id int(11)   nOt NuLl, create_consumers JSON NuLl, created_date datetime nOt NuLl dEfAuLt CURRENT_TIMESTAMP() , CONSTRAINT fk_operators_create_consumers_operator_api_key_id FOREIGN KEY (operator_api_key_id ) REFERENCES api_keys (id) , CONSTRAINT fk_operators_create_consumers_operator_ip_address_id  FOREIGN KEY (operator_ip_address_id ) REFERENCES operator_ip_addresses (id) , CONSTRAINT uq_operator_api_key_id_operator_ip_address_id UNIQUE (operator_api_key_id, operator_ip_address_id));"#;
+        let ant_farmer = AntFarmer::from(MySqlDialect {});
+        let expected = r#"CREATE TABLE operators_create_consumers (
+    operator_api_key_id    INT(11)  NOT NULL                            
+  , operator_ip_address_id INT(11)  NOT NULL                            
+  , create_consumers       JSON         NULL                            
+  , created_date           datetime NOT NULL DEFAULT CURRENT_TIMESTAMP()
+  , CONSTRAINT fk_operators_create_consumers_operator_api_key_id    FOREIGN KEY (operator_api_key_id)                         REFERENCES api_keys              (id)
+  , CONSTRAINT fk_operators_create_consumers_operator_ip_address_id FOREIGN KEY (operator_ip_address_id)                      REFERENCES operator_ip_addresses (id)
+  , CONSTRAINT uq_operator_api_key_id_operator_ip_address_id        UNIQUE      (operator_api_key_id, operator_ip_address_id)
 )
 ;"#;
 
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use sqlparser::dialect::MySqlDialect;
 
 #[allow(unused_variables)]
 fn main() {
-    let sql = r#"cReAtE tAbLe operators_create_consumers (operator_api_key_id    int(11)    NOT NULL, operator_ip_address_id int(11)   nOt NuLl, create_consumers JSON NuLl, created_date datetime nOt NuLl dEfAuLt CURRENT_TIMESTAMP());"#;
+    let sql = r#"cReAtE tAbLe operators_create_consumers (operator_api_key_id    int(11)    NOT NULL, operator_ip_address_id int(11)   nOt NuLl, create_consumers JSON NuLl, created_date datetime nOt NuLl dEfAuLt CURRENT_TIMESTAMP() , CONSTRAINT fk_operators_create_consumers_operator_api_key_id FOREIGN KEY (operator_api_key_id ) REFERENCES api_keys (id) , CONSTRAINT fk_operators_create_consumers_operator_ip_address_id  FOREIGN KEY (operator_ip_address_id ) REFERENCES operator_ip_addresses (id), CONSTRAINT uq_operator_api_key_id_operator_ip_address_id UNIQUE (operator_api_key_id, operator_ip_address_id));"#;
 
     let ant_farmer = AntFarmer::from(MySqlDialect {});
 
